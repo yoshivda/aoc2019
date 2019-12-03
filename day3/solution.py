3531fa8e4cb10aca41a280151105d7f4a68849ea
Yoshi van den Akker <y.a.w.vandenakker@student.tudelft.nl>
2019-12-03 12:01:07 +0100

    Day 3 part 2

--- day3/solution.py
+++ day3/solution.py
@@ -4,34 +4,61 @@ def solve(filename):
     with open(filename) as f:
         wire1ops = f.readline()
         wire2ops = f.readline()
-        wire1 = coordinates(wire1ops)
-        wire2 = coordinates(wire2ops)
-        cross = wire1.intersection(wire2)
-        cross.remove((0, 0))
-        return min(map(lambda x: abs(x[0]) + abs(x[1]), cross))
+        wire1coords, wire1steps = coordinates(wire1ops)
+        wire2coords, wire2steps = coordinates(wire2ops)
+        cross = wire1coords.intersection(wire2coords)
+        if (0, 0) in cross:
+            cross.remove((0, 0))
+        return min(map(lambda x: wire1steps[x] + wire2steps[x], cross))
 
 
 def coordinates(ops):
-    res = set()
+    coords = set()
+    stepsdict = dict()
+    steps = 0
     x = y = 0
     ops = ops.split(",")
     for op in ops:
         dir = op[0]
         num = int(op[1:])
         if dir == "U":
-            res = res.union({(x, y + i) for i in range(num)})
-            y += num
+            for _ in range(num):
+                y += 1
+                steps += 1
+                coords.add((x, y))
+                if (x, y) in stepsdict:
+                    stepsdict[(x, y)] = min(steps, stepsdict[(x, y)])
+                else:
+                    stepsdict[(x, y)] = steps
         elif dir == "R":
-            res = res.union({(x + i, y) for i in range(num)})
-            x += num
+            for _ in range(num):
+                x += 1
+                steps += 1
+                coords.add((x, y))
+                if (x, y) in stepsdict:
+                    stepsdict[(x, y)] = min(steps, stepsdict[(x, y)])
+                else:
+                    stepsdict[(x, y)] = steps
         elif dir == "D":
-            res = res.union({(x, y - i) for i in range(num)})
-            y -= num
+            for _ in range(num):
+                y -= 1
+                steps += 1
+                coords.add((x, y))
+                if (x, y) in stepsdict:
+                    stepsdict[(x, y)] = min(steps, stepsdict[(x, y)])
+                else:
+                    stepsdict[(x, y)] = steps
         elif dir == "L":
-            res = res.union({(x - i, y) for i in range(num)})
-            x -= num
+            for _ in range(num):
+                x -= 1
+                steps += 1
+                coords.add((x, y))
+                if (x, y) in stepsdict:
+                    stepsdict[(x, y)] = min(steps, stepsdict[(x, y)])
+                else:
+                    stepsdict[(x, y)] = steps
 
-    return res
+    return coords, stepsdict
 
 
 if __name__ == "__main__":
--- day3/test.py
+++ day3/test.py
@@ -4,13 +4,13 @@ from .solution import solve
 
 class TestSuite(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(159, solve("day3/test1.txt"))
+        self.assertEqual(610, solve("day3/test1.txt"))
 
     def test_2(self):
-        self.assertEqual(135, solve("day3/test2.txt"))
+        self.assertEqual(410, solve("day3/test2.txt"))
 
     def test_3(self):
-        self.assertEqual(6, solve("day3/test3.txt"))
+        self.assertEqual(30, solve("day3/test3.txt"))
 
 
 if __name__ == "__main__":
