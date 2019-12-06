b07d9ffbed37c2a3908064ba0f12ae5ddbf561fb
Yoshi van den Akker <y.a.w.vandenakker@student.tudelft.nl>
2019-12-06 10:59:08 +0100

    Day 6 pt2

--- day6/solution.py
+++ day6/solution.py
@@ -1,4 +1,4 @@
-def solve(filename):
+def read(filename):
     orbits = dict()
     planets = set()
     with open(filename) as f:
@@ -8,7 +8,24 @@ def solve(filename):
             planets.add(a)
             planets.add(b)
             orbits[b] = a
+    return orbits, planets
+
+
+def solve1(filename):
+    orbits, planets = read(filename)
     return sum(calc_orbits(orbits, p) for p in planets)
+
+
+def solve2(filename):
+    orbits, _ = read(filename)
+    trace_you = trace_orbits(orbits, "YOU")
+    trace_san = trace_orbits(orbits, "SAN")
+    min_steps = 1000000000
+    for i in range(len(trace_you)):
+        for j in range(len(trace_san)):
+            if trace_you[i] == trace_san[j] and i + j < min_steps:
+                min_steps = i + j
+    return min_steps - 2
 
 
 amount_orbits = {"COM": 0}
@@ -21,5 +38,11 @@ def calc_orbits(orbits, p):
         return 1 + calc_orbits(orbits, orbits[p])
 
 
+def trace_orbits(orbits, p):
+    if p == "COM":
+        return []
+    return [p] + trace_orbits(orbits, orbits[p])
+
+
 if __name__ == "__main__":
-    print(solve("input.txt"))
+    print(solve2("input.txt"))
