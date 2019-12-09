740d221b373d6e34f90e8e1c967ef54b29c1ead7
Yoshi van den Akker <y.a.w.vandenakker@student.tudelft.nl>
2019-12-09 13:27:37 +0100

    Day 8 pt2

--- day8/solution.py
+++ day8/solution.py
@@ -1,4 +1,4 @@
-def solve(filename, width, height):
+def solve1(filename, width, height):
     with open(filename) as f:
         row = f.readline().strip()
         layers = [row[x:x+width*height] for x in range(0, len(row), width*height)]
@@ -6,5 +6,23 @@ def solve(filename, width, height):
         return layer.count("1") * layer.count("2")
 
 
+def solve2(filename, width, height):
+    with open(filename) as f:
+        row = f.readline().strip()
+        layers = [row[x:x+width*height] for x in range(0, len(row), width*height)]
+        res = ""
+        for i in range(width*height):
+            for layer in layers:
+                if layer[i] == "0":
+                    res += " "
+                    break
+                elif layer[i] == "1":
+                    res += "#"
+                    break
+        res = [res[x:x+width] for x in range(0, len(row), width)]
+        return "\n".join(res)
+
+
 if __name__ == "__main__":
-    print(solve("input.txt", 25, 6))
+    # print(solve1("input.txt", 25, 6))
+    print(solve2("input.txt", 25, 6))
--- day8/testsuite.py
+++ day8/testsuite.py
@@ -1,10 +1,13 @@
 import unittest
-from .solution import solve
+from .solution import solve1, solve2
 
 
 class TestSuite(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(1, solve("day8/test1.txt", 3, 2))
+        self.assertEqual(1, solve1("day8/test1.txt", 3, 2))
+
+    def test_2(self):
+        self.assertEqual("0110", solve2("day8/test2.txt", 2, 2))
 
 
 if __name__ == "__main__":
