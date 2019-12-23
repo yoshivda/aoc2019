84f79ef26b60c5169411c6a75b846e560f94507b
Yoshi van den Akker <y.a.w.vandenakker@student.tudelft.nl>
2019-12-23 16:43:08 +0100

    Day 22 pt2

--- day22/solution.py
+++ day22/solution.py
@@ -16,6 +16,33 @@ def solve(filename):
     return cards
 
 
+def solve2(filename):
+    position = 2020
+    size = 119315717514047
+    iterations = 101741582076661
+    offset = 0
+    increment = 1
+    with open(filename) as f:
+        for row in f.readlines():
+            if row.startswith("deal into"):
+                increment *= -1
+                increment %= size
+                offset += increment
+                offset %= size
+            else:
+                num = list(map(int, re.findall(r"[-0-9]+", row)))[0]
+                if row.startswith("cut"):
+                    offset += increment * num
+                    offset %= size
+                else:
+                    increment *= pow(num, -1, size)
+                    increment %= size
+
+    increment_total = pow(increment, iterations, size)
+    offset = offset * (1 - increment_total) * pow(1 - increment, -1, size)
+    return (offset + increment_total * position) % size
+
+
 def cut(cards, n):
     return cards[n:] + cards[:n]
 
@@ -30,4 +57,4 @@ def deal_inc(cards, n):
 
 
 if __name__ == "__main__":
-    print(solve("input.txt").index(2019))
+    print(solve2("input.txt"))
